c26de02e341f9cbf8f383e6014c7ccaa07ff572a
Maixent Butery <maixent.butery20@gmail.com>
2024-09-23 11:53:59 +0200

    Création d'un nouveau projet versionné

---
+++ TP1_manipNombresInt_NOM1/src/tp1_manipnombresint_nom1/TP1_manipNombresInt_Butery.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_manipnombresint_nom1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Maixent
+ */
+public class TP1_manipNombresInt_Butery {
+
+    
+    public static void main(String[] args) {    
+    int VariableA = 0;
+    int VariableB = 0;
+    System.out.println("Veuiller saisir 2 entiers :");
+    Scanner scanner = new Scanner(System.in);
+    VariableA = scanner.nextInt();
+    VariableB = scanner.nextInt();
+    }
+    
+}
