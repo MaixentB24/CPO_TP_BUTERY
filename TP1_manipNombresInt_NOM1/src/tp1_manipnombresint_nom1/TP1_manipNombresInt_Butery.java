6df6a63635254f8f1a7bafa8ed57b262de613225
Maixent Butery <maixent.butery20@gmail.com>
2024-09-23 12:02:49 +0200

    Création d'un nouveau projet versionné

--- TP1_manipNombresInt_NOM1/src/tp1_manipnombresint_nom1/TP1_manipNombresInt_Butery.java
+++ TP1_manipNombresInt_NOM1/src/tp1_manipnombresint_nom1/TP1_manipNombresInt_Butery.java
@@ -20,6 +20,12 @@ public class TP1_manipNombresInt_Butery {
     Scanner scanner = new Scanner(System.in);
     VariableA = scanner.nextInt();
     VariableB = scanner.nextInt();
+    System.out.println("La variable a est :"+VariableA);
+    System.out.println("La variable b est :"+VariableB);
+    System.out.println("La somme des 2 variables est :"+(VariableA+VariableB));
+    System.out.println("La différence des 2 variables est :"+(VariableA-VariableB));
+    System.out.println("Le produit des 2 variables est :"+(VariableA*VariableB));
+    System.out.println("La divion euclidienne des 2 variables donne :"+(VariableA/VariableB)+" il reste :"+(VariableA%VariableB));
     }
     
 }
