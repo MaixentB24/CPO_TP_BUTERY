66da04dc70b06db422f57de2f09263031d858a4e
Maixent Butery <maixent.butery20@gmail.com>
2024-09-27 11:10:21 +0200

    Création d'un nouveau projet versionné

--- TP1_guessMyNumber_BUTERY/src/tp1_guessmynumber_butery/TP1_guessMyNumber_BUTERY.java
+++ TP1_guessMyNumber_BUTERY/src/tp1_guessmynumber_butery/TP1_guessMyNumber_BUTERY.java
@@ -5,6 +5,7 @@
 package tp1_guessmynumber_butery;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -16,13 +17,34 @@ public class TP1_guessMyNumber_BUTERY {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    /**Random generateurAleat = new Random();
+    *for (int i=0;i<=5;i++){
+    *    int n = generateurAleat.nextInt(100);
+    *System.out.print("\n"+n);
+    * 
+    *}
+    */
     Random generateurAleat = new Random();
-    for (int i=0;i<=5;i++){
-        int n = generateurAleat.nextInt(100);
-    System.out.print("\n"+n);
+    int n = generateurAleat.nextInt(100);
+    int m=0;
+    int w=0;
+    System.out.println("Veuillez choisir une difficulté :");
+    while (n!=m){
         
+    System.out.println("Veuillez saisir un nombre : ");
+    Scanner scanner = new Scanner(System.in);
+    m = scanner.nextInt();
+    w+=1;
+    if (n==m){
+        System.out.println("Gagné");
+    System.out.println("vous avez fait "+w+" Tentative");
     }
-    
+    else if (n<m){
+        System.out.println("trop grand !");
     }
-    
+    else if (n>m){
+        System.out.println("trop petit");
+    }
+    }
+    }
 }
